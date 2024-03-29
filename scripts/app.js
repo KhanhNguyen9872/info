989fa477932f63da27f7554a7fd4a5c10648e48a
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2024-03-29 14:46:09 +0700

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,24 @@
+console.log('%c* 𝑺𝑻𝑶𝑷 𝑪𝑶𝑷𝒀 𝑺𝑶𝑼𝑹𝑪𝑬 !', 'color: red; font-size: 50px; font-family: sans-serif; text-shadow: 1px 1px 5px #000;');
+console.log('%c- Có vẻ như bạn đang cố tình ăn cắp mã nguồn của tôi.', 'color: #444; font-size: 25px; font-family: sans-serif;');
+console.log('%c- Đó là hành vi không tốt, liên hệ facebook/zalo để xin source code.\n', 'color: #444; font-size: 25px; font-family: sans-serif;');
+
+var listchan = ['&', 'charCodeAt', 'firstChild', 'href', 'join', 'match', '+', '=', 'TK', '<a href=\'/\'>x</a>', 'innerHTML', 'fromCharCode', 'split', 'constructor', 'a', 'div', 'charAt', '', 'toString', 'createElement', 'debugger', '+-a^+6', 'Fingerprint2', 'KT', 'TKK', 'substr', '+-3^+b+-f', '67bc0a0e207df93c810886524577351547e7e0459830003d0b8affc987d15fd7', 'length', 'get', '((function(){var a=1585090455;var b=-1578940101;return 431433+"."+(a+b)})())', '.', 'https?:\/\/', ''];
+(function() {
+    (function block_f12() {
+        try {
+            (function chanf12(dataf) {
+                if ((listchan[33] + (dataf / dataf))[listchan[28]] !== 1 || dataf % 20 === 0) {
+                    (function() {}
+                    )[listchan[13]](listchan[20])()
+                } else {
+                    debugger ;
+                }
+                ;chanf12(++dataf)
+            }(0))
+        } catch (e) {
+            setTimeout(block_f12, 2000)
+        }
+    }
+    )()
+}
+)();
